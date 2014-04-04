2e2564b125040b1824e934cd9943dd97cf81e811
codehop.dev <codehop.dev@4a73f0ba-03bc-26ed-5ec3-fd41fe5d7a7c>
2014-04-04 16:28:00 +0000

    backing up last functining core NER code before porting to the new NCBO API

---
+++ src/java/ncbo/AnnotatorClient-DEPRECATED.java
@@ -0,0 +1,89 @@
+package ncbo;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.httpclient.params.HttpMethodParams;
+
+public class AnnotatorClient { 
+	
+	public static final String annotatorUrl = "http://rest.bioontology.org/obs/annotator";
+	private static final String API_KEY = "74028721-e60e-4ece-989b-1d2c17d14e9c";
+	
+    public static void main( String[] args ) {
+    	String contents = annotate("water");
+    	System.out.println(contents);
+    }
+    
+    public static String annotate(String text) {
+    	try {
+            HttpClient client = new HttpClient();
+            client.getParams().setParameter(
+            HttpMethodParams.USER_AGENT,"Annotator Client Example - Annotator");  //Set this string for your application
+            
+            String stopwords = getStopWords(new File("data/stopwords.txt"));
+            
+            PostMethod method = new PostMethod(annotatorUrl);
+            
+            // Configure the form parameters
+            method.addParameter("longestOnly","true");
+            method.addParameter("wholeWordOnly","true");
+            method.addParameter("filterNumber", "true");
+            method.addParameter("stopWords",stopwords);
+            method.addParameter("withDefaultStopWords","false");
+            method.addParameter("isTopWordsCaseSensitive","false");
+            method.addParameter("mintermSize","3");
+            method.addParameter("scored", "true");
+            method.addParameter("withSynonyms","true"); 
+            method.addParameter("ontologiesToExpand", "");
+            method.addParameter("ontologiesToKeepInResult", ""); 
+            method.addParameter("isVirtualOntologyId", "true"); 
+            method.addParameter("semanticTypes", ""); 
+            method.addParameter("levelMax", "0");
+            method.addParameter("mappingTypes", ""); //null, Automatic, Manual 
+            method.addParameter("textToAnnotate", text);  //"Melanoma is a malignant tumor of melanocytes which are found predominantly in skin but also in the bowel and the eye");
+            method.addParameter("format", "xml"); //Options are 'text', 'xml', 'tabDelimited'   
+            method.addParameter("apikey", API_KEY);
+
+            // Execute the POST method
+            int statusCode = client.executeMethod(method);
+            
+            if( statusCode != -1 ) {
+                try {
+                String contents = method.getResponseBodyAsString();
+                method.releaseConnection();
+                return contents;
+                }
+                catch( Exception e ) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        catch( Exception e ){
+            e.printStackTrace();
+        }
+    	return null;
+    }
+    
+    private static String getStopWords(File stopWordsFile) {
+    	String contents = "";
+    	try {
+    		BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(stopWordsFile)));
+    		String line;
+    		while ((line = reader.readLine()) != null) {
+    			contents += line + ",";
+    		}
+    	} catch (IOException ioe) {
+    		ioe.printStackTrace();
+    		System.exit(1);
+    	}
+
+    	return contents;
+    }
+}
+
